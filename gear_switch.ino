085ef1782137b7f6fa4794287bac0ae98228114d
Fionn O'Connell-Hussey <64333406+Fionnoch@users.noreply.github.com>
2022-07-18 01:40:19 +0100

    Update gear_switch.ino
    new pins assigned for 7 segment display.
    new method of controlling esc on/off relays. Arduino now handles that rather than switch.
    lot more work on switch debouncing to try and get rid of gear 1-4 hopping problem caused when esc switch turned on/off in non ideal way. 
    
    Cleaning and smoother timings required

--- gear_switch.ino
+++ gear_switch.ino
@@ -9,41 +9,46 @@ const int down_button_pin = 3;     // the number of the pushbutton pin
 const int turbo_state_pin = 4;
 const int esc_state_pin = 5;
 
-const int gear_control_1 =  6;      //
-const int gear_control_2 =  7;      //
-const int turbo_control =  8;      //
-const int clutch_control =  10;      //
+const int gear_control_1 =  7;      //
+const int gear_control_2 =  8;      //
+const int turbo_control =  6;      //
+const int ecu_control = 9;
+const int clutch_control = 10;      //
 
-const int display_pin_1 = A7;
-const int display_pin_2 = A6;
-const int display_pin_3 = A5;
-const int display_pin_4 = A4;
-const int display_pin_5 = A3;
-const int display_pin_6 = A2;
-const int display_pin_7 = A1;
-const int display_pin_8 = 13;
+const int display_pin_8 = 12; //g
+const int display_pin_7 = A0; //f
+const int display_pin_2 = A5; //a
+const int display_pin_3 = A4; //b
+const int display_pin_1 = A3; //dp
+const int display_pin_4 = A2; //c
+const int display_pin_5 = A1; //d
+const int display_pin_6 = 13; //e
 
-const int segment_display[10][7] = {    { 1, 1, 1, 1, 1, 1, 0 }, // 0
-  { 0, 1, 1, 0, 0, 0, 0 }, // 1
-  { 1, 1, 0, 1, 1, 0, 1 }, // 2
-  { 1, 1, 1, 1, 0, 0, 1 }, // 3
-  { 0, 1, 1, 0, 0, 1, 1 }, // 4
-  { 1, 0, 1, 1, 0, 1, 1 }, // 5
-  { 1, 0, 1, 1, 1, 1, 1 }, // 6
-  { 1, 1, 1, 0, 0, 0, 0 }, // 7
-  { 1, 1, 1, 1, 1, 1, 1 }, // 8
-  { 1, 1, 1, 0, 0, 1, 1 }
-};   // 9
+const int segment_display[10][8] = {    
+  { 1, 1, 1, 1, 1, 1, 0, 1 }, // 0
+  { 0, 1, 1, 0, 0, 0, 0, 1 }, // 1
+  { 1, 1, 0, 1, 1, 0, 1, 1 }, // 2
+  { 1, 1, 1, 1, 0, 0, 1, 1 }, // 3
+  { 0, 1, 1, 0, 0, 1, 1, 1 }, // 4
+  { 1, 0, 1, 1, 0, 1, 1, 1 }, // 5
+  { 1, 0, 1, 1, 1, 1, 1, 1 }, // 6
+  { 1, 1, 1, 0, 0, 0, 0, 1 }, // 7
+  { 1, 1, 1, 1, 1, 1, 1, 1 }, // 8
+  { 1, 1, 1, 0, 0, 1, 1, 1 }  // 9
+};   
 
 //const int segment_pin_var[8] = {display_pin_1, display_pin_2, display_pin_3, display_pin_4, display_pin_5, display_pin_6, display_pin_7, display_pin_8};
-const int segment_pin_var[8] = {A7, A6, A5, A4, A3, A2, A1, 13};
+//const int segment_pin_var[8] = {A7, A6, A5, A4, A3, A2, A1, 13};
 
 // variables
 int buttonState = 0;         // variable for reading the pushbutton status
 unsigned int last_interupt;
 
+const int gear_trigger_delay = 1000;
+
 int set_gear = 1;
 int current_gear = 1;
+bool allow_update = true;
 
 void setup() {
 
@@ -51,8 +56,9 @@ void setup() {
   // initialize the pins as an output:
   pinMode(gear_control_1, OUTPUT);
   pinMode(gear_control_2, OUTPUT);
-  pinMode(turbo_control, OUTPUT);
+  pinMode(turbo_control,  OUTPUT);
   pinMode(clutch_control, OUTPUT);
+  pinMode(ecu_control, OUTPUT);
 
   pinMode(display_pin_1, OUTPUT);
   pinMode(display_pin_2, OUTPUT);
@@ -74,14 +80,21 @@ void setup() {
 
   analogWrite(clutch_control, 255); //enguage the clutch
 
+  digitalWrite(gear_control_1, HIGH); //set to 1st gear
+  digitalWrite(gear_control_2, LOW);
   write_num(1);
+
+  digitalWrite(ecu_control, LOW);
 
   Serial.println(set_gear);
 }
 
 void loop() {
 
-  if ( esc_state_pin ) { 
+  if ( digitalRead(esc_state_pin) == HIGH ) { 
+
+    digitalWrite(ecu_control, LOW);
+    
 
     //check if the turbo is turned on
     if (digitalRead(turbo_state_pin) == HIGH) {
@@ -90,16 +103,25 @@ void loop() {
       digitalWrite(turbo_control, LOW);
     }
 
-    // if the user presses a change gear button
+    
 
+    if (millis()-last_interupt > gear_trigger_delay){
+      allow_update = true;
+    }
+
+    // if the user presses a change gear button
     if (set_gear != current_gear) { //as opposed to doing a update system, check whether the gears are at the correct settings
+
+      allow_update = false;
 
       if (abs(set_gear - current_gear) > 1) {
         Serial.println("skipping gear");
         if (set_gear > current_gear) {
           Serial.println("skipping gear upwards");
+          set_gear = current_gear +1;
         } else {
           Serial.println("skipping gear downwards");
+          set_gear = current_gear - 1;
         }
       }
 
@@ -125,7 +147,7 @@ void loop() {
         write_num(3);
         current_gear = 3;
       }
-      else if (set_gear == 4) {
+      else if (set_gear == 4) { //4th gear
         digitalWrite(gear_control_1, HIGH);
         digitalWrite(gear_control_2, HIGH);
         write_num(4);
@@ -134,9 +156,13 @@ void loop() {
       
       //reenguage the clutch
       clutch_engage();
+      allow_update = true;
       Serial.print("Current gear is ");
       Serial.println(current_gear);
     }
+  } else {
+    digitalWrite(ecu_control, HIGH);
+    allow_update = false;
   }
   delay(100); //how often the loop runs
 }
@@ -147,28 +173,36 @@ void loop() {
 
 void gear_up() //function to detect gear change up
 {
-  if ((millis() - last_interupt) > 100) {
+  Serial.print("allow_update = ");
+  Serial.println(allow_update);  
+  if (((millis() - last_interupt) > gear_trigger_delay ) && allow_update==true) {
     //Update count
-    detachInterrupt(digitalPinToInterrupt(up_button_pin));
+    //detachInterrupt(digitalPinToInterrupt(up_button_pin));
     if (set_gear < 4) {
       set_gear++;
     }
-    attachInterrupt(digitalPinToInterrupt(up_button_pin), gear_up, FALLING);
+    
     last_interupt = millis();
+    //attachInterrupt(digitalPinToInterrupt(up_button_pin), gear_up, FALLING);
     Serial.println("gear set to ");
     Serial.println(set_gear);
+    allow_update = false;
   }
 }
 
 void gear_down() //function to detect gear change down
 {
-  if ((millis() - last_interupt) > 100) {
-    detachInterrupt(digitalPinToInterrupt(down_button_pin));
+  Serial.print("allow_update = ");
+  Serial.println(allow_update);
+  if (((millis() - last_interupt) > gear_trigger_delay) && allow_update==true) {
+    //detachInterrupt(digitalPinToInterrupt(down_button_pin));
     if (set_gear > 1) {
       set_gear = set_gear - 1;
     }
-    attachInterrupt(digitalPinToInterrupt(down_button_pin), gear_down, FALLING);
+    
     last_interupt = millis();
+    allow_update = false;
+    //attachInterrupt(digitalPinToInterrupt(down_button_pin), gear_down, FALLING);
     Serial.println("Set to gear ");
     Serial.println(set_gear);
   }
@@ -182,7 +216,7 @@ void clutch_disengage() //function to detect gear change down
 
 void clutch_engage() //function to detect gear change down
 {
-  analogWrite(clutch_control, 255); //255 = 12V, 0 = 0V ... 191 approx = 9V
+  analogWrite(clutch_control, 255); //255 = 12V, 0 = 0V .... 191 approx = 9V
   delay(500); //delay to allow solonoid to close
 }
 
@@ -191,13 +225,13 @@ void write_num(int number)
 {
   //for (int j=0; j < 7; j  ) {
   //digitalWrite(segment_pin_var[j], segment_display[number][j]);
-  digitalWrite(display_pin_1, segment_display[number][0] );
-  digitalWrite(display_pin_2, segment_display[number][1] );
-  digitalWrite(display_pin_3, segment_display[number][2] );
-  digitalWrite(display_pin_4, segment_display[number][3] );
-  digitalWrite(display_pin_5, segment_display[number][4] );
-  digitalWrite(display_pin_6, segment_display[number][5] );
-  digitalWrite(display_pin_7, segment_display[number][6] );
-  digitalWrite(display_pin_8, segment_display[number][7] );
   //}
+  digitalWrite(display_pin_1, segment_display[number][7] ); //pin 1 is the dot 
+  digitalWrite(display_pin_2, segment_display[number][0] );
+  digitalWrite(display_pin_3, segment_display[number][1] );
+  digitalWrite(display_pin_4, segment_display[number][2] );
+  digitalWrite(display_pin_5, segment_display[number][3] );
+  digitalWrite(display_pin_6, segment_display[number][4] );
+  digitalWrite(display_pin_7, segment_display[number][5] );
+  digitalWrite(display_pin_8, segment_display[number][6] );
 }
